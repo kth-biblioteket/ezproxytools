9430f374bed4f43fe1445749a824cd804e2baf09
Thomas Lind <tholind@kth.se>
2025-11-07 12:26:20 +0100

    säkerhet

--- VerifyToken.js
+++ VerifyToken.js
@@ -9,9 +9,14 @@ function verifyToken(req, res, next) {
         || req.headers['authorization']
         || req.headers['kth-ug-token']
         || req.cookies.jwt
+    const isApiCall = req.headers['x-api-call'] === 'true';
 
     if (!token)
-        return res.render('login',{logindata: {"status":"ok", "message":"No token"}})
+        if (isApiCall) {
+            return res.status(401).send("Unauthorized");
+        } else {
+            return res.render('login',{logindata: {"status":"ok", "message":"No token"}})
+        }
 
     if (req.headers['x-access-token'] || req.cookies.jwt) {
         jwt.verify(token, process.env.SECRET, async function (err, decoded) {
